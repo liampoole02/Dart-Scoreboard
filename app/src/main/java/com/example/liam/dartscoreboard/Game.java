5cbe63d0994e78bc30f6395bf9d279f562432d4c
LIAM <liampoole02@gmail.com>
2021-04-19 01:55:45 +0300

    Scores can be subtracted

---
+++ app/src/main/java/com/example/liam/dartscoreboard/Game.java
@@ -0,0 +1,30 @@
+package com.example.liam.dartscoreboard;
+
+public class Game {
+    private int total1;
+    private int total2;
+
+    public Game() {
+
+    }
+    public Game(int total1, int total2) {
+        this.total1 = total1;
+        this.total2 = total2;
+    }
+
+    public int getTotal1() {
+        return total1;
+    }
+
+    public void setTotal1(int total1) {
+        this.total1 = total1;
+    }
+
+    public int getTotal2() {
+        return total2;
+    }
+
+    public void setTotal2(int total2) {
+        this.total2 = total2;
+    }
+}
--- app/src/main/java/com/example/liam/dartscoreboard/MainActivity.java
+++ app/src/main/java/com/example/liam/dartscoreboard/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.liam.dartscoreboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,12 +33,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private TextView player1remain;
     private TextView player2remain;
 
+    private LinearLayout player1layout;
+    private LinearLayout player2layout;
+
     private int game;
+
+    private int total1;
+    private int total2;
+
+    private Game scores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scores=new Game();
 
         player1=findViewById(R.id.player1);
         player2=findViewById(R.id.player2);
@@ -65,11 +77,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         player1remain=findViewById(R.id.player1remain);
         player2remain=findViewById(R.id.player2remain);
 
+        player1layout=findViewById(R.id.player1layout);
+        player2layout=findViewById(R.id.player2layout);
+
+        scores.setTotal1(0);
+        scores.setTotal2(0);
+
         add1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 game=Integer.parseInt(spinner.getSelectedItem().toString());
-                int total=0;
 
                 if(player1score.getText().toString().equals("") || spinner.getSelectedItem()==null){
                     Toast.makeText(MainActivity.this, "Please enter required fields", Toast.LENGTH_SHORT).show();
@@ -77,10 +94,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     player1Array.add(Integer.parseInt(player1score.getText().toString()));
 
                     for(int x: player1Array){
-                        total=total+x;
+                        total1=total1+x;
                     }
-                    player1remain.setText(game-total+"");
+
+                    player1remain.setText(game-total1+"");
+
+                    scores.setTotal1(game-total1);
                     player1score.setText("");
+
+                    if(scores.getTotal1()<scores.getTotal2()){
+                        player1layout.setBackgroundColor(Color.GREEN);
+                        player2layout.setBackgroundColor(Color.RED);
+                    }else{
+                        player2layout.setBackgroundColor(Color.GREEN);
+                        player1layout.setBackgroundColor(Color.RED);
+                    }
                 }
             }
         });
@@ -89,18 +117,31 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             @Override
             public void onClick(View view) {
                 game=Integer.parseInt(spinner.getSelectedItem().toString());
-                int total=0;
 
                 if(player2score.getText().toString().equals("") || spinner.getSelectedItem()==null){
                     Toast.makeText(MainActivity.this, "Please enter required fields", Toast.LENGTH_SHORT).show();
                 }else{
                     player2Array.add(Integer.parseInt(player2score.getText().toString()));
 
-                    for(int x: player1Array){
-                        total=total+x;
+                    for(int x: player2Array){
+                        total2=total2+x;
                     }
-                    player2remain.setText(game-total+"");
+
+                    player2remain.setText(game-total2+"");
+                    scores.setTotal2(game-total2);
+
+                    Toast.makeText(MainActivity.this, ""+scores.getTotal1()+" "+scores.getTotal2(), Toast.LENGTH_SHORT).show();
+
+
                     player2score.setText("");
+
+                    if(scores.getTotal1()<scores.getTotal2()){
+                        player1layout.setBackgroundColor(Color.GREEN);
+                        player2layout.setBackgroundColor(Color.RED);
+                    }else{
+                        player2layout.setBackgroundColor(Color.GREEN);
+                        player1layout.setBackgroundColor(Color.RED);
+                    }
                 }
             }
         });
@@ -109,7 +150,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//        Toast.makeText(this, ""+spinner.getSelectedItem(), Toast.LENGTH_SHORT).show();
 
     }
 
