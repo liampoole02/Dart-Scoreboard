4829032b3c62141079bfdd30afd441cbaa772998
LIAM <liampoole02@gmail.com>
2021-04-28 01:44:12 +0300

    3 players functionality

--- app/src/main/java/com/example/liam/dartscoreboard/Game.java
+++ app/src/main/java/com/example/liam/dartscoreboard/Game.java
@@ -3,15 +3,25 @@ package com.example.liam.dartscoreboard;
 public class Game {
     private int total1;
     private int total2;
+    private int total3;
 
     public Game() {
 
     }
-    public Game(int total1, int total2) {
+    public Game(int total1, int total2, int total3) {
         this.total1 = total1;
         this.total2 = total2;
+        this.total3 = total3;
+
     }
 
+    public int getTotal3() {
+        return total3;
+    }
+
+    public void setTotal3(int total3) {
+        this.total3 = total3;
+    }
 
     public int getTotal1() {
         return total1;
--- app/src/main/java/com/example/liam/dartscoreboard/ThreePlayer.java
+++ app/src/main/java/com/example/liam/dartscoreboard/ThreePlayer.java
@@ -3,6 +3,7 @@ package com.example.liam.dartscoreboard;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -20,39 +21,51 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ThreePlayer extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private EditText player1;
     private EditText player2;
+    private EditText player3;
+
     Spinner spinner;
 
     private EditText player1score;
     private EditText player2score;
+    private EditText player3score;
 
     private TextView player1scores;
     private TextView player2scores;
+    private TextView player3scores;
 
     private ArrayList<Integer> player1Array;
     private ArrayList<Integer> player2Array;
+    private ArrayList<Integer> player3Array;
 
     private Button add1;
     private Button add2;
+    private Button add3;
+
     private Button reset;
 
     private Button undop1;
     private Button undop2;
+    private Button undop3;
 
     private TextView player1remain;
     private TextView player2remain;
+    private TextView player3remain;
 
     private LinearLayout player1layout;
     private LinearLayout player2layout;
+    private LinearLayout player3layout;
 
     private int game;
 
     private int total1;
     private int total2;
+    private int total3;
 
     private Game scores;
 
@@ -61,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.three_player);
 
         ActionBar actionBar;
         actionBar = getSupportActionBar();
@@ -72,15 +85,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         player1=findViewById(R.id.player1);
         player2=findViewById(R.id.player2);
+        player3=findViewById(R.id.player3);
 
         player1score=findViewById(R.id.player1score);
         player2score=findViewById(R.id.player2score);
+        player3score=findViewById(R.id.player3score);
 
         add1=findViewById(R.id.add1);
         add2=findViewById(R.id.add2);
+        add3=findViewById(R.id.add3);
 
         undop1=findViewById(R.id.undop1);
         undop2=findViewById(R.id.undop2);
+        undop3=findViewById(R.id.undop3);
 
         reset=findViewById(R.id.reset);
 
@@ -98,20 +115,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         player1Array=new ArrayList();
         player2Array=new ArrayList();
+        player3Array=new ArrayList();
 
         player1remain=findViewById(R.id.player1remain);
         player2remain=findViewById(R.id.player2remain);
+        player3remain=findViewById(R.id.player3remain);
 
         player1layout=findViewById(R.id.player1layout);
         player2layout=findViewById(R.id.player2layout);
+        player3layout=findViewById(R.id.player3layout);
 
         player1scores=findViewById(R.id.player1scores);
         player2scores=findViewById(R.id.player2scores);
+        player3scores=findViewById(R.id.player3scores);
 
         leader=findViewById(R.id.leader);
 
         scores.setTotal1(0);
         scores.setTotal2(0);
+        scores.setTotal3(0);
 
         game = Integer.parseInt(spinner.getSelectedItem().toString());
 
@@ -120,11 +142,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             public void onClick(View view) {
                 game = Integer.parseInt(spinner.getSelectedItem().toString());
 
-                if(player1Array.size()==player2Array.size()) {
+                if(player1Array.size()==player2Array.size()&&player2Array.size()==player3Array.size()) {
                     total1=0;
 
                     if (player1score.getText().toString().equals("") || spinner.getSelectedItem() == null) {
-                        Toast.makeText(MainActivity.this, "Please enter required fields", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ThreePlayer.this, "Please enter required fields", Toast.LENGTH_SHORT).show();
                     } else {
                         player1Array.add(Integer.parseInt(player1score.getText().toString()));
 
@@ -137,26 +159,29 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                         player1score.setText("");
 
-                        if (scores.getTotal1() < scores.getTotal2()) {
-                            player1layout.setBackgroundColor(Color.GREEN);
-                            player2layout.setBackgroundColor(Color.RED);
-                        } else if(scores.getTotal1() == scores.getTotal2()){
-                            player2layout.setBackgroundColor(Color.GRAY);
-                            player1layout.setBackgroundColor(Color.GRAY);
-                        }else {
-                            player2layout.setBackgroundColor(Color.GREEN);
-                            player1layout.setBackgroundColor(Color.RED);
-                        }
+//                        if (scores.getTotal1() < scores.getTotal2()) {
+//                            player1layout.setBackgroundColor(Color.GREEN);
+//                            player2layout.setBackgroundColor(Color.RED);
+//                        } else if(scores.getTotal1() == scores.getTotal2()){
+//                            player2layout.setBackgroundColor(Color.GRAY);
+//                            player1layout.setBackgroundColor(Color.GRAY);
+//                        }else {
+//                            player2layout.setBackgroundColor(Color.GREEN);
+//                            player1layout.setBackgroundColor(Color.RED);
+//                        }
                     }
 
                     player1scores.setText(""+playerScoresList(player1Array));
 
-                }else {
-                    Toast.makeText(MainActivity.this, "Player 2 turn", Toast.LENGTH_SHORT).show();
+                }else if(player1Array.size()>player2Array.size()){
+                    Toast.makeText(ThreePlayer.this, "Player 2 turn", Toast.LENGTH_SHORT).show();
+                    player1score.setText("");
+                }else{
+                    Toast.makeText(ThreePlayer.this, "Player 3 turn", Toast.LENGTH_SHORT).show();
                     player1score.setText("");
                 }
 
-                leader();
+//                leader();
             }
         });
 
@@ -169,45 +194,94 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                 if(player1Array.size()!=player2Array.size()) {
 
-                if (player2score.getText().toString().equals("") || spinner.getSelectedItem() == null) {
-                    Toast.makeText(MainActivity.this, "Please enter required fields", Toast.LENGTH_SHORT).show();
-                } else {
-                    player2Array.add(Integer.parseInt(player2score.getText().toString()));
+                    if (player2score.getText().toString().equals("") || spinner.getSelectedItem() == null) {
+                        Toast.makeText(ThreePlayer.this, "Please enter required fields", Toast.LENGTH_SHORT).show();
+                    } else {
+                        player2Array.add(Integer.parseInt(player2score.getText().toString()));
 
-                    for (int x : player2Array) {
-                        total2 = total2 + x;
+                        for (int x : player2Array) {
+                            total2 = total2 + x;
+                        }
+
+                        scores.setTotal2(game - total2);
+                        player2remain.setText(scores.getTotal2() + "");
+
+                        player2score.setText("");
+
+//                        if (scores.getTotal1() < scores.getTotal2()) {
+//                            player1layout.setBackgroundColor(Color.GREEN);
+//                            player2layout.setBackgroundColor(Color.RED);
+//                        } else if(scores.getTotal1() == scores.getTotal2()){
+//                            player2layout.setBackgroundColor(Color.GRAY);
+//                            player1layout.setBackgroundColor(Color.GRAY);
+//                        }
+//                        else {
+//                            player2layout.setBackgroundColor(Color.GREEN);
+//                            player1layout.setBackgroundColor(Color.RED);
+//                        }
+
                     }
-
-                    scores.setTotal2(game - total2);
-                    player2remain.setText(scores.getTotal2() + "");
-
-                    player2score.setText("");
-
-                    if (scores.getTotal1() < scores.getTotal2()) {
-                        player1layout.setBackgroundColor(Color.GREEN);
-                        player2layout.setBackgroundColor(Color.RED);
-                    } else if(scores.getTotal1() == scores.getTotal2()){
-                        player2layout.setBackgroundColor(Color.GRAY);
-                        player1layout.setBackgroundColor(Color.GRAY);
-                    }
-                    else {
-                        player2layout.setBackgroundColor(Color.GREEN);
-                        player1layout.setBackgroundColor(Color.RED);
-                    }
-                }
 
                     player2scores.setText(""+playerScoresList(player2Array));
 
-                    leader();
+//                    leader();
 
-            }else{
-                    Toast.makeText(MainActivity.this, "Player 1 turn", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(ThreePlayer.this, "Player 3 turn", Toast.LENGTH_SHORT).show();
                     player2score.setText("");
 
                 }
             }
         });
 
+        add3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                total3=0;
+
+                game = Integer.parseInt(spinner.getSelectedItem().toString());
+
+                if(player2Array.size()>player3Array.size()) {
+
+                    if (player3score.getText().toString().equals("") || spinner.getSelectedItem() == null) {
+                        Toast.makeText(ThreePlayer.this, "Please enter required fields", Toast.LENGTH_SHORT).show();
+                    } else {
+                        player3Array.add(Integer.parseInt(player3score.getText().toString()));
+
+                        for (int x : player3Array) {
+                            total3 = total3 + x;
+                        }
+
+                        scores.setTotal3(game - total3);
+                        player3remain.setText(scores.getTotal3() + "");
+
+                        player3score.setText("");
+
+//                        if (scores.getTotal1() < scores.getTotal2()) {
+//                            player1layout.setBackgroundColor(Color.GREEN);
+//                            player2layout.setBackgroundColor(Color.RED);
+//                        } else if(scores.getTotal1() == scores.getTotal2()){
+//                            player2layout.setBackgroundColor(Color.GRAY);
+//                            player1layout.setBackgroundColor(Color.GRAY);
+//                        }
+//                        else {
+//                            player2layout.setBackgroundColor(Color.GREEN);
+//                            player1layout.setBackgroundColor(Color.RED);
+//                        }
+
+                    }
+
+                    player3scores.setText(""+playerScoresList(player3Array));
+
+//                    leader();
+
+                }else{
+                    Toast.makeText(ThreePlayer.this, "Player 1 turn", Toast.LENGTH_SHORT).show();
+                    player3score.setText("");
+
+                }
+            }
+        });
 
 
         reset.setOnClickListener(new View.OnClickListener() {
@@ -235,7 +309,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                 player1scores.setText(""+playerScoresList(player1Array));
 
-                Toast.makeText(MainActivity.this, "Player 1 turn, enter correct score", Toast.LENGTH_LONG).show();
+                Toast.makeText(ThreePlayer.this, "Player 1 turn, enter correct score", Toast.LENGTH_LONG).show();
 
             }
 
@@ -258,12 +332,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 player2scores.setText(""+playerScoresList(player2Array));
 
 
-                Toast.makeText(MainActivity.this, "Player 2 turn, enter correct score", Toast.LENGTH_LONG).show();
+                Toast.makeText(ThreePlayer.this, "Player 2 turn, enter correct score", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        undop3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                total3=0;
+
+                player3Array.remove(player3Array.size()-1);
+
+                for (int x : player3Array) {
+                    total3 = total3 + x;
+                }
+
+                scores.setTotal3(game-total3);
+                player3remain.setText(scores.getTotal3() + "");
+
+                player3scores.setText(""+playerScoresList(player3Array));
+
+
+                Toast.makeText(ThreePlayer.this, "Player 3 turn, enter correct score", Toast.LENGTH_LONG).show();
 
             }
         });
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -275,10 +372,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()){
             case R.id.twoplayer:
-                startActivity(new Intent(MainActivity.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(new Intent(ThreePlayer.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                 return true;
             case R.id.threeplayer:
-                startActivity(new Intent(MainActivity.this, ThreePlayer.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(new Intent(ThreePlayer.this, ThreePlayer.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                 return true;
         }
         return false;
@@ -290,7 +387,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         String t="";
         for (int strTemp : tail){
-            t=t+" > "+strTemp;
+            t=t+">"+strTemp;
         }
         return t;
     }
